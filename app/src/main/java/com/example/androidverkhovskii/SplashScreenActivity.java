9a6faed6d2d87bb4fe7588112ad69b9288eb6e01
Aleksandr Verkhovskii <verhoffsky@mail.ru>
2019-05-10 10:57:44 +0300

    removed the unnecessary code from the files SplashScreenActivity.java and activity_splash_screen.xml

--- app/src/main/java/com/example/androidverkhovskii/SplashScreenActivity.java
+++ app/src/main/java/com/example/androidverkhovskii/SplashScreenActivity.java
@@ -1,13 +1,9 @@
 package com.example.androidverkhovskii;
 
-import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-
 
 public class SplashScreenActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
